60535c37c2e579034a58af1ba485bf9a8ad10f43
duongpham <hongduong5441@gmail.com>
2022-12-07 23:40:56 +0700

    Upload

---
+++ NewsProject/src/java/Models/CategoryModel.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Models;
+
+/**
+ *
+ * @author hongd
+ */
+public class CategoryModel {
+    private int id;
+    private String categoryName;
+
+    public CategoryModel() {
+    }
+
+    public CategoryModel(int id, String categoryName) {
+        this.id = id;
+        this.categoryName = categoryName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+    
+    
+}
---
+++ NewsProject/src/java/Models/CommentModel.java
@@ -0,0 +1,81 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Models;
+
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author hongd
+ */
+public class CommentModel {
+    private int id;
+    private int newsId;
+    private String comment;
+    private LocalDateTime createDate;
+    private String createUser;
+    private LocalDateTime updateDate;
+
+    public CommentModel() {
+    }
+
+    public CommentModel(int id, int newsId, String comment, LocalDateTime createDate, String createUser, LocalDateTime updateDate) {
+        this.id = id;
+        this.newsId = newsId;
+        this.comment = comment;
+        this.createDate = createDate;
+        this.createUser = createUser;
+        this.updateDate = updateDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getNewsId() {
+        return newsId;
+    }
+
+    public void setNewsId(int newsId) {
+        this.newsId = newsId;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public LocalDateTime getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(LocalDateTime createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getCreateUser() {
+        return createUser;
+    }
+
+    public void setCreateUser(String createUser) {
+        this.createUser = createUser;
+    }
+
+    public LocalDateTime getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(LocalDateTime updateDate) {
+        this.updateDate = updateDate;
+    }
+    
+}
